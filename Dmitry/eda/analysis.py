a163a1c3592536e20094a444dcd6fe25a2fefde1
dsaperov <dsaperov.md@gmail.com>
2023-11-13 22:27:29 +0300

    Add analysis of RGB channels for AKIEC & BKL classes

--- Dmitry/eda/analysis.py
+++ Dmitry/eda/analysis.py
@@ -52,3 +52,77 @@ plt.title("Image Sizes (pixels)")
 plt.savefig("image_sizes")
 
 
+# -----------------------
+# RGB channels analysis |
+# -----------------------
+def plot_rgb_distributions(image_class: str, r: np.array, g: np.array, b: np.array) -> None:
+    plt.figure()
+
+    plt.plot(r, color="red")
+    plt.plot(g, color="green")
+    plt.plot(b, color="blue")
+
+    plt.title(f"Color Plot of {image_class} class")
+    plt.xlabel("Color value")
+    plt.ylabel("Pixel count")
+
+    plt.savefig(os.path.join("classes", image_class, "pixel_value_distributions" + ".png"))
+
+
+def get_variance(r_avg: np.array, g_avg: np.array, b_avg: np.array,
+                 image_series: pd.Series) -> Tuple[np.array, np.array, np.array]:
+    image_count = image_series.size
+    r_square_dev, g_square_dev, b_square_dev = np.zeros((3, 450, 600), dtype=np.float64)
+    for image_name in image_series:
+        image = Image.open(os.path.join(IMAGE_FOLDER_PATH, image_name + ".jpg"))
+
+        r_array, g_array, b_array = [np.array(channel) for channel in image.split()]
+        r_square_dev += (r_array - r_avg) ** 2
+        g_square_dev += (g_array - g_avg) ** 2
+        b_square_dev += (b_array - b_avg) ** 2
+
+    return r_square_dev / image_count, g_square_dev / image_count, b_square_dev / image_count
+
+
+def write_to_csv(image_class: str, filenames: Tuple[str, str, str],
+                 arrays: Tuple[np.array, np.array, np.array]) -> None:
+    for filename, arr in zip(filenames, arrays):
+        np.savetxt(os.path.join("classes", image_class, filename + ".csv"), arr, delimiter=',')
+
+
+image_classes = ["AKIEC", "BKL"]
+for image_class in image_classes:
+    r_values, g_values, b_values = np.zeros((3, 256), dtype=int)
+    r_total, g_total, b_total = np.zeros((3, 450, 600), dtype=int)
+
+    # Generate a pandas Series object with the names of images belonging to the specified class
+    image_series = df.loc[df[image_class] == 1, "image"]
+    for image_name in image_series:
+        image = Image.open(os.path.join(IMAGE_FOLDER_PATH, image_name + ".jpg"))
+
+        r_array, g_array, b_array = [np.array(channel) for channel in image.split()]
+
+        # Count the number of occurrences of each value in channel arrays and accumulate them
+        r_values += np.bincount(r_array.flatten(), minlength=256)
+        g_values += np.bincount(g_array.flatten(), minlength=256)
+        b_values += np.bincount(b_array.flatten(), minlength=256)
+
+        r_total, g_total, b_total = r_total + r_array, g_total + g_array, b_total + b_array
+
+    # Plot a histogram of the aggregated pixel intensities for the specified class
+    plot_rgb_distributions(image_class, r_values, g_values, b_values)
+
+    image_count = image_series.size
+
+    # Calculate average of pixel values for each of the RGB channels
+    r_avg, g_avg, b_avg = r_total / image_count, g_total / image_count, b_total / image_count
+    write_to_csv(image_class, ("r_average", "g_average", "b_average"), (r_avg, g_avg, b_avg))
+
+    # Calculate variance of pixel values for each of the RGB channels
+    r_var, g_var, b_var = get_variance(r_avg, g_avg, b_avg, image_series)
+    write_to_csv(image_class, ("r_variance", "g_variance", "b_variance"), (r_var, g_var, b_var))
+
+    # Combine the color channels
+    rgb_avg = np.stack([r_avg, g_avg, b_avg], axis=-1)
+
+    plt.imsave(os.path.join("classes", image_class, "average_image" + ".png"), rgb_avg.astype("uint8"))
