b2edd924ca4414649e1e541d7b70499ddae01446
Aleksei Kaliagin <80597622+lesssch@users.noreply.github.com>
2024-01-12 12:34:19 +0300

    Feature/aleksey mvp models (#5)
    * Add model and api
    
    * Add model and api
    
    * Add model and api
    
    * Add CNN model
    
    * Add directories for models and their APIs
    
    ---------
    
    Co-authored-by: Aleksei Kaliagin <>

---
+++ Aleksey/MVP models/SGD API/main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI, File, UploadFile
+from skimage.feature import hog
+from skimage.transform import rescale
+import numpy as np
+import joblib
+from PIL import Image
+from io import BytesIO
+import pandas as pd
+
+model = joblib.load("../SGD Model/sgd_model.pkl")
+pca = joblib.load("../SGD Model/pca.pkl")
+scaler = joblib.load("../SGD Model/sc.pkl")
+
+class_dict = {"MEL": 1, "NV": 2, "BCC": 3, "AKIEC": 4, "BKL":5, "DF": 6, "VASC": 7}
+
+app = FastAPI()
+
+
+@app.post("/predict_item")
+def predict_item(file: UploadFile = File(...)) -> str:
+    image_vectors = []
+    content = file.file.read()
+    image = Image.open(BytesIO(content))
+    gray_image = np.asarray(image.convert("L"))
+
+    image = rescale(gray_image, 1/3, mode='reflect')
+    img_hog, hog_img = hog(
+        image, pixels_per_cell=(14, 14),
+        cells_per_block=(2, 2),
+        orientations=9,
+        visualize=True,
+        block_norm='L2-Hys')
+    flat_vector = np.array(hog_img).flatten()
+    image_vectors.append(flat_vector)
+    image_vectors_array = np.array(image_vectors)
+    image_vectors_array = pca.transform(image_vectors_array)
+    df = pd.DataFrame(data=image_vectors_array)
+    df = scaler.transform(df)
+
+    prediction = model.predict(df)
+    result = None
+    for key, value in class_dict.items():
+        if value == prediction:
+            result = key
+            break
+
+    return result
