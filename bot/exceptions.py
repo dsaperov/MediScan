1a455bd630426de69bf44d8310de9b7a7c21847f
dsaperov <dsaperov.md@gmail.com>
2024-01-14 13:30:33 +0300

    Modify UnconfiguredEnvironmentError message

--- bot/exceptions.py
+++ bot/exceptions.py
@@ -6,7 +6,5 @@ class UnconfiguredEnvironmentError(Exception):
         Args:
             :param str var_name: name of the environment variable that could not be detected
         """
-        msg = f'The environment variable {var_name} is not set in the system. Please execute the command ' \
-              f'"cp .env.example .env" and set the values for the variables in the ".env" file, or ' \
-              f'set these environment variables in the operating system in any other way.'
+        msg = f'The environment variable {var_name} is not set in the system.'
         super().__init__(msg)
