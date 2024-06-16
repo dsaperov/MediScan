3f7ad8bdfe6df5c6edafd8792d36e434c804f6f3
dsaperov <dsaperov.md@gmail.com>
2024-06-16 04:36:47 +0300

    Update tests for the bot after MySQL integration

--- bot/tests/test_bot.py
+++ bot/tests/test_bot.py
@@ -1,8 +1,7 @@
-import os
 import random
 import string
 from io import BytesIO
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, MagicMock, AsyncMock
 import pickle
 
 import pytest
@@ -57,7 +56,7 @@ async def test_cmd_help():
     /help - get the list of all possible commands
     /predict - get the prediction which class is most likely
     /predictmel - get the probability that this is melanoma
-    /predictcnn - get the class prediction using CNN
+    /predictcnn - get the class prediction using CNN (efficient_net_b0)
     /rate - rate this bot
     /showrating - show bot rating
                 """
@@ -109,7 +108,7 @@ async def test_predict():
     requester = MockedBot(MessageHandler(download_photo))
     calls = await requester.query(MESSAGE_WITH_PHOTO.as_object())
     answer_message = calls.send_message.fetchone().text
-    assert answer_message == "The probability of VASC = 6.0 %"
+    assert answer_message == "The most likely class is BCC."
 
     requester = MockedBot(MessageHandler(cmd_predictmel))
     calls = await requester.query(MESSAGE.as_object(text="/predictmel"))
@@ -125,14 +124,22 @@ async def test_predict():
 @pytest.mark.asyncio
 async def test_rating():
     requester = MockedBot(MessageHandler(clear_rate))
-    calls = await requester.query(MESSAGE.as_object(text="/clearrating"))
-    answer_message = calls.send_message.fetchone().text
-    assert answer_message == "Rating cleared."
+    mock_delete_rating = AsyncMock()
+    with patch("bot.delete_rating", mock_delete_rating):
+        with patch("bot.check_rating", return_value=False):
+            calls = await requester.query(MESSAGE.as_object(text="/clearrating"))
+            answer_message = calls.send_message.fetchone().text
+            assert answer_message == "No rating to clear."
+        with patch("bot.check_rating", return_value=True):
+            calls = await requester.query(MESSAGE.as_object(text="/clearrating"))
+            answer_message = calls.send_message.fetchone().text
+            assert answer_message == "Rating cleared."
 
-    requester = MockedBot(MessageHandler(show_rate))
-    calls = await requester.query(MESSAGE.as_object(text="/showrate"))
-    answer_message = calls.send_message.fetchone().text
-    assert answer_message == "No ratings given yet."
+    with patch("bot.get_ratings", return_value=[]):
+        requester = MockedBot(MessageHandler(show_rate))
+        calls = await requester.query(MESSAGE.as_object(text="/showrate"))
+        answer_message = calls.send_message.fetchone().text
+        assert answer_message == "No ratings given yet."
 
     requester = MockedBot(MessageHandler(rate))
     calls = await requester.query(MESSAGE.as_object(text="/rate"))
@@ -146,11 +153,11 @@ async def test_add_rating():
     mock_bot = MagicMock(spec=Bot)
     mock_message = MagicMock()
     mock_callback_query.message = mock_message
+    mock_save_rating = AsyncMock()
 
-    with (patch('bot.get_ratings', return_value=[]),
-          patch('builtins.open', create=True),
-          patch('pickle.dump'),
-          patch.object(mock_bot, 'edit_message_text') as mock_edit_message_text):
+    with (patch('bot.save_rating', mock_save_rating),
+          patch.object(mock_bot, 'edit_message_text') as mock_edit_message_text,
+          patch('bot.get_ratings', return_value=[3])):
         await add_rating(mock_callback_query, mock_bot)
 
         mock_edit_message_text \
@@ -162,21 +169,3 @@ async def test_add_rating():
         calls = await requester.query(MESSAGE.as_object(text="/showrate"))
         answer_message = calls.send_message.fetchone().text
         assert answer_message == "Mean rating is 3.00 using 1 rating entries."
-
-
-def test_ratings_file_creation(monkeypatch):
-    """Test that the ratings file is created if it doesn't exist."""
-    ratings_file = "ratings.pkl"
-    ratings_file_renamed = "ratings.pkl.bak"
-
-    if os.path.exists(ratings_file):
-        os.rename(ratings_file, ratings_file_renamed)
-
-    try:
-        exec(open("bot.py").read())
-        assert os.path.exists(ratings_file)
-        if os.path.exists(ratings_file):
-            os.remove(ratings_file)
-    finally:
-        if os.path.exists(ratings_file_renamed):
-            os.rename(ratings_file_renamed, ratings_file)
